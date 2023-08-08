4f7c024452717f4fc45f8d1ea706e88d7c58eaa0
s66829020@gmail.com <s66829020@gmail.com>
2023-08-08 21:23:52 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player_Team_Requirement_Project
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            T20Team t20Team = new T20Team();
+
+            while (true)
+            {
+                Console.WriteLine("\nWelcome to Cricket T20 Management System");
+                Console.WriteLine("Enter your choice: \n1: To Add Player \n2: To Remove Player by Id \n3: Get Player By Id \n4: Get Player by Name \n5: Get All Players \n6: Exit");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Input Player Id:");
+                        int playerId = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Input Player Name:");
+                        string playerName = Console.ReadLine();
+
+                        Console.WriteLine("Input Player Age:");
+                        int playerAge = int.Parse(Console.ReadLine());
+
+                        Player newPlayer = new Player
+                        {
+                            PlayerId = playerId,
+                            PlayerName = playerName,
+                            PlayerAge = playerAge
+                        };
+                        t20Team.Add(newPlayer);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Enter Player Id to remove:");
+                        int playerIdToRemove = int.Parse(Console.ReadLine());
+                        t20Team.Remove(playerIdToRemove);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Enter Player Id to retrieve:");
+                        int playerIdToRetrieve = int.Parse(Console.ReadLine());
+                        Player retrievedPlayerById = t20Team.GetPlayerById(playerIdToRetrieve);
+                        if (retrievedPlayerById != null)
+                            Console.WriteLine($"Player found: Id={retrievedPlayerById.PlayerId}, Name={retrievedPlayerById.PlayerName}, Age={retrievedPlayerById.PlayerAge}");
+                        else
+                            Console.WriteLine("Player not found.");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Enter Player Name to retrieve:");
+                        string playerNameToRetrieve = Console.ReadLine();
+                        Player retrievedPlayerByName = t20Team.GetPlayerByName(playerNameToRetrieve);
+                        if (retrievedPlayerByName != null)
+                            Console.WriteLine($"Player found: Id={retrievedPlayerByName.PlayerId}, Name={retrievedPlayerByName.PlayerName}, Age={retrievedPlayerByName.PlayerAge}");
+                        else
+                            Console.WriteLine("Player not found.");
+                        break;
+
+                    case 5:
+                        var allPlayers = t20Team.GetAllPlayers();
+                        Console.WriteLine("All Players:");
+                        foreach (var player in allPlayers)
+                        {
+                            Console.WriteLine($"Id={player.PlayerId}, Name={player.PlayerName}, Age={player.PlayerAge}");
+                        }
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Exiting...");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Try again.");
+                     break;
+                }
+            }
+        }
+    }
+}
---
+++ T20Team.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player_Team_Requirement_Project
+{
+    public class T20Team
+    {
+        public static List<Player> T20team = new List<Player>();
+
+        public T20Team()
+        {
+            T20team.Capacity = 11;
+        }
+
+        public void Add(Player player)
+        {
+            if (T20team.Count >= 11)
+            {
+                Console.WriteLine("Team is already full. Can't add more players.");
+                return;
+            }
+
+            T20team.Add(player);
+            Console.WriteLine("Player added successfully.");
+        }
+
+        public void Remove(int playerId)
+        {
+            Player playerToRemove = T20team.Find(p => p.PlayerId == playerId);
+            if (playerToRemove != null)
+            {
+                T20team.Remove(playerToRemove);
+                Console.WriteLine("Player removed successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Player not found in the team.");
+            }
+        }
+
+        public Player GetPlayerById(int playerId)
+        {
+            return T20team.Find(p => p.PlayerId == playerId);
+        }
+
+        public Player GetPlayerByName(string playerName)
+        {
+            return T20team.Find(p => p.PlayerName == playerName);
+        }
+
+        public List<Player> GetAllPlayers()
+        {
+            return T20team;
+        }
+    }
+}
